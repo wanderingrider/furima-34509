13c5a80648fb2204a140f6c2279273f9110167ba
wanderingrider <rotarywr250@gmail.com>
2021-02-24 15:25:49 +0900

    バリデーション、モデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,        presence: true
+  validates :surname,         presence: true
+  validates :first_name,      presence: true
+  validates :read_surname,    presence: true
+  validates :read_first_name, presence: true
+  validates :birthday,        presence: true 
 end
--- db/migrate/20210224051555_devise_create_users.rb
+++ db/migrate/20210224051555_devise_create_users.rb
@@ -4,10 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.nickname.string
-
+      t.string :nickname,           null: false
       t.string :email,              null: false, default: "", unique: true
       t.string :encrypted_password, null: false, default: ""
+      t.string :surname,            null: false
+      t.string :first_name,         null: false
+      t.string :read_surname,       null: false
+      t.string :read_first_name,    null: false
+      t.date   :birthday,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
