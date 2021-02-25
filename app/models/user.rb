91a8eb5681416d09aed8fa0030cc5523f181b61e
wanderingrider <rotarywr250@gmail.com>
2021-02-25 11:45:26 +0900

    Rspec,Factory.Bot導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,12 @@ class User < ApplicationRecord
   with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
    validates :surname   
    validates :first_name
+  end
+
+ with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } do
    validates :read_surname
    validates :read_first_name
-end
+ end
 
 PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
 validates_format_of :password, with: PASSWORD_REGEX
