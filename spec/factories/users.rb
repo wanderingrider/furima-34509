d07a04a101e2ff1af25479bdd83c1764ebd21fc1
wanderingrider <rotarywr250@gmail.com>
2021-02-25 15:02:22 +0900

    FactoryBotの準備

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    surname               {Faker::Name.last_name}
+    first_name            {Faker::Name.first_name}
+    read_surname          {'ヤマダ'}
+    read_first_name       {'タロウ'}
+    birthday              {Faker::Date.birthday(min_age: 5, max_age: 90)}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe '新規登録/ユーザー情報' do
+    it 'nicknameが必須のこと' do
+    end
+    it 'emailが必須のこと' do
+    end
+    it 'メールアドレスが一意性であること' do
+    end
+    it 'メールアドレスは、@を含むこと' do
+    end
+    it 'passwordが必須のこと' do
+    end
+     it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+    end
+    it 'passwordは、半角英数字での入力が必須であること' do
+    end
+    it 'passwordは確認用を含めて２回入力すること' do
+    end
+    it 'paswordとpassword(確認用)は値の一致が必須あること' do 
+    end
+ end
+
+ describe '新規登録/本人情報確認'
+   it 'ユーザー本名は、名字と名前がそれぞれ必須であること' do
+   end
+   it  'ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力が必須であること' do
+   end
+   it 'ユーザー本名のフリガナは、名字と名前がそれぞれ必須であること' do
+   end
+   it 'ユーザー本名のフリガナは、全角（カタカナ）での入力が必須であること' do
+   end
+   it '生年月日が必須であること' do
+   end
+ end
+
+ describe 'トップページ'
+   it 'ログアウト状態では、ヘッダーに新規登録/ログインボタンが表示されること' do
+   end
+   it 'ログイン状態では、ヘッダーにユーザーのニックネーム/ログアウトボタンが表示されること' do
+   end 
+   it 'ヘッダーの新規登録/ログインボタンをクリックすることで、各ページに遷移できること' do
+   end
+   it 'ヘッダーのログアウトボタンをクリックすることで、ログアウトができること' do
+   end
+  end
+
+
 end
