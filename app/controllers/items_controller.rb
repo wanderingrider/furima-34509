917740e761f5bda3724b6649567713cf0d3165c8
wanderingrider <rotarywr250@gmail.com>
2021-02-28 19:07:59 +0900

    保存機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
   private
   
   def item_params
-    params.require(:item).permit(:product_name, :description, :category_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:product_name, :description, :category_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, :product_status_id, :price, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,27 @@
 class Item < ApplicationRecord
-  validates :product_name,       presence: true
-  validates :description,        presence: true
-  validates :category_id,        numericality: { other_than: 1 } 
-  validates :Product_status_id,  numericality: { other_than: 1 } 
-  validates :shipping_charge_id, numericality: { other_than: 1 } 
-  validates :prefecture_id,      numericality: { other_than: 1 } 
-  validates :shipping_date_id,   numericality: { other_than: 1 } 
-  validates :price,              presence: true
+
+  with_options presence: true do
+    validates :product_name
+    validates :description
+    validates :image
+  end
+
+  with_options numericality: { other_than: 1 } do
+   validates :category_id
+   validates :product_status_id
+   validates :shipping_charge_id
+   validates :prefecture_id
+   validates :shipping_date_id
+  end
+
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with:/\A[0-9]+\z/ }
   
   has_one_attached :image
   belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  belongs_to :Product_status
+  belongs_to :product_status
   belongs_to :shipping_charge
   belongs_to :prefecture
   belongs_to :shipping_date
