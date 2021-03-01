4b06dc252b2a91a714335e2120b6222a92a76348
wanderingrider <rotarywr250@gmail.com>
2021-03-01 17:46:12 +0900

    ルーティング・コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,8 +21,12 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
 
   private
 
