66618f22420e981ce7246f9e8abc727fffd15df4
wanderingrider <rotarywr250@gmail.com>
2021-03-02 10:23:02 +0900

    商品情報編集機能　修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,7 +47,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
+    @item = Item.find(params[:id])
     redirect_to action: :index unless item.user.id == current_user.id
   end
 end
