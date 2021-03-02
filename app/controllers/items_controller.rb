99c70175f9805c714abdefe65382aba05b83b424
wanderingrider <rotarywr250@gmail.com>
2021-03-02 12:16:45 +0900

    ルーティング・コントローーラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy ]
   before_action :authenticate_user!, only: [:new, :edit, :create, :update]
   before_action :move_to_index, only: [:edit, :update]
 
@@ -33,6 +33,11 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy 
+    @item.destroy
+    redirect_to root_path
   end
 
   private
