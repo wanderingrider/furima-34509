be751207cca9ab8ae258efbec74b65f4f1dd5a21
wanderingrider <rotarywr250@gmail.com>
2021-03-03 14:39:23 +0900

    ルーティング設定2

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to:"items#index"
   
   resources :items do
-    resources :shipping, only: [:create, :index]
+    resources :shippings, only: [:index, :create, :new]
   end
 end
