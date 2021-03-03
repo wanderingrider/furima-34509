7ffc8b18212786c165865e581d72240244d93fec
wanderingrider <rotarywr250@gmail.com>
2021-03-03 11:28:51 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"items#index"
   
-  resources :items
-  
+  resources :items do
+    resources :shipping, only: [:create, :index]
+  end
 end
